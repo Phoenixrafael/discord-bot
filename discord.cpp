ac44ddb440691d6666e4f51795791d9fb0decc52
Phoenix Rafael <sieul08@naver.com>
2021-10-30 23:49:41 +0900

    a

--- discord.cpp
+++ discord.cpp
@@ -396,7 +396,7 @@ public:
 			ibot::MultipleAnswer M;
 			while (readFile) {
 				getline(readFile, str);
-				printf("-%s-\n", str.c_str());
+				printf("- %s - - %s -\n", str.c_str(), hexprint(str).c_str());
 				if (str == "{{answer}}"
 					|| str == "{{channelid}}"
 					|| str == "{{roleid}}"
