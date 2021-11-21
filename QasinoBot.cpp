928bfa4a5a9728ec9e1747a3be10778f5fc97718
Phoenix Rafael <sieul08@naver.com>
2021-11-21 22:57:43 +0900

    dice error fix

--- QasinoBot.cpp
+++ QasinoBot.cpp
@@ -968,7 +968,7 @@ public:
 			response.data.flags = InteractionAppCommandCallbackData::Flags::Ephemeral;
 			createInteractionResponse(interaction, interaction.token, response);
 			qasino::qambler Qb = readQamblerInfo(interaction.member.ID);
-			RollDice(interaction.channelID, false, 3, GetTextL("dice-title-w-nick", Qb.nick.c_str()));
+			RollDice(interaction.channelID, false, 3, GetTextL("dice-title-w-nick", Qb.nick.c_str())); 
 		}
 		else {
 
