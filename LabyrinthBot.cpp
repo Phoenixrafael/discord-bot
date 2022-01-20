e78cc5e4bb2900410971075e4f82db4bdee0fa89
Phoenix Rafael <sieul08@naver.com>
2022-01-20 10:32:59 +0900

    resolved error

--- LabyrinthBot.cpp
+++ LabyrinthBot.cpp
@@ -1651,6 +1651,16 @@ public:
 	}
 };
 
+std::string replaceAll(std::string& str, std::string from, std::string to) {
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+	{
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+	return str;
+}
+
 std::string GetToken(const char* key, ...) {
 	std::string ret = GetTextA(key);
 	replaceAll(ret, "\n", "");
